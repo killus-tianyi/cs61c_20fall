59ce5282fe4d254a46a85f968a3c836ec3bdff22
killua_tianyi <22723042@bjtu.edu.cn>
2024-07-29 17:38:24 +0800

    7/29

--- proj/su24-proj2-starter-main/src/matmul.s
+++ proj/su24-proj2-starter-main/src/matmul.s
@@ -26,12 +26,12 @@
 matmul:
 
     # Error checks
-    bne a2 a3 error_match
+    bne a2 a4 error_match
     li t2 1
     blt a1 t2 error_length
     blt a2 t2 error_length
-    blt a3 t2 error_length
     blt a4 t2 error_length
+    blt a5 t2 error_length
     
     # Prologue
     addi sp sp -8
@@ -48,10 +48,10 @@ matmul:
 
 
 outer_loop_start:
-ebreak
     beq t0 a1 outer_loop_end
+    ebreak
     inner_loop_start:
-        beq t1 a2 inner_loop_end
+        beq t1 a5 inner_loop_end
         addi sp sp -32
         sw a0 28(sp)
         sw a1 24(sp)
@@ -61,18 +61,20 @@ ebreak
         sw t0 8(sp)
         sw t1 4(sp)
         sw t2 0(sp)
+        
 
         add a1 x0 a3
         add a2 x0 a2
         addi a3 x0 1
         add a4 x0 a5
 
-        jal dot
+        jal ra dot
         sw a0 0(s0)
         addi s0 s0 4
 
         lw t2 0(sp) 
         lw t1 4(sp) 
+        lw t0 8(sp)
         lw a5 12(sp)
         lw a3 16(sp)
         lw a2 20(sp)
